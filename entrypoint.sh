e5babfefdde7f110e84da64597d09f1caa7907d4
erkan000 <erkan000@hotmail.com>
2022-06-13 22:59:06 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ wget https://dl.minio.io/server/minio/release/linux-amd64/minio
 chmod +x minio
 export MINIO_ROOT_USER="$USER"
 export MINIO_ROOT_PASSWORD="$PASSWORD"
-./minio server --address :$PORT --console-address :443 /mnt/minio/data
+./minio server --address :$PORT /mnt/minio/data
